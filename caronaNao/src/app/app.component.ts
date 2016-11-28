6ec1a3f0bd3241d81aefb9350e211dfbc85e14ad
Carlos Macêdo <carlosmacedo025@gmail.com>
2016-11-28 18:58:59 -0200

    All views
    All necessary screens for mvc

--- caronaNao/src/app/app.component.ts
+++ caronaNao/src/app/app.component.ts
@@ -3,18 +3,29 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen,SQLite} from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 
+import { DevedoresPage } from '../pages/devedores/devedores';
+import { AddPessoasPage } from '../pages/add-pessoas/add-pessoas';
+import { ViagemAddPessoasPage } from '../pages/viagem-add-pessoas/viagem-add-pessoas';
+import { ViagemFimPage } from '../pages/viagem-fim/viagem-fim';
+import { ViagemPropriedadesPage } from '../pages/viagem-propriedades/viagem-propriedades';
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: `./app.html`
 })
 export class MyApp {
-  rootPage = HomePage;
+  devedores = DevedoresPage;
+  addPessoas = AddPessoasPage;
+  viagemAddPessoas = ViagemAddPessoasPage;
+  viagemFim = ViagemFimPage;
+  viagemPropriedades = ViagemPropriedadesPage;
+
+  rootPage = this.viagemPropriedades;
   public db:SQLite;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
 
-      
+
       this.db= new SQLite();
       this.db.openDatabase({
               name: "data.db",
@@ -23,11 +34,11 @@ export class MyApp {
               this.db.executeSql("CREATE TABLE IF NOT EXISTS passageiro (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT,celular TEXT,divida REAL)", {}).then((data) => {
                   alert("TABLE CREATED");
                   console.log("TABLE CREATED: ", data);
-                  
+
             }, (error) => {
                   alert("erro");
                   console.error("Unable to execute sql", error);
-                  
+
             })
           }, (error) => {
               console.error("Unable to open database", error);
@@ -41,5 +52,9 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(opcao) {
+    this.rootPage = opcao;
   }
 }
